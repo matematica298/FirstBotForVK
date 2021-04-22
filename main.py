24fff7a7dcbced8ebcaf8ffb87c3751ae098d8de
matematica298 <shop_spy@mail.ru>
2021-04-22 22:44:53 +0300

    Somethind edited

--- main.py
+++ main.py
@@ -33,6 +33,13 @@ async def hi_handler(message: Message, text: str):
     dz = text
     await message.answer(f"Вы записали {text!r}")
 
+@bot.on.message(text="Добавить в дз <text>")
+async def hi_handler(message: Message, new_slovo: str):
+global dz
+dz = dz + new_slovo
+await message.answer(f"Вы записали {new_slovo!r}")
+
+
 
 @bot.on.message(text="Дз на завтра")
 async def hi_handler(message: Message):
