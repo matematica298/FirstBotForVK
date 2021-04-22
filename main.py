80afe4cd1f94fadb764091536ca05b23d8f885ac
matematica298 <shop_spy@mail.ru>
2021-04-22 22:13:55 +0300

    Changed register for commands

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ bot = Bot(token="042069c40f987a3c04a68c1524d19484d56d9089f8ca2dfd84bbd93e05d50cb
 bot.labeler.vbml_ignore_case = True
 
 
-@bot.on.message(text="Привет")
+@bot.on.message(text=["привет", 'ку'])
 async def hi_handler(message: Message):
     # Создаем клавиатуру
     KEYBOARD = Keyboard(one_time=True)
@@ -21,15 +21,17 @@ async def hi_handler(message: Message):
     # Перенос на новую строчку
     # KEYBOARD.row()
 
-    # Получаем JSON-развертку клавиатуры
-
-    KEYBOARD = KEYBOARD.get_json()
-    await message.answer("Привет", keyboard=KEYBOARD)
+    await message.answer("Привет", keyboard=KEYBOARD.get_json())
 
 
 @bot.on.message(text="Записать <text>")
 async def hi_handler(message: Message, text: str):
     await message.answer(f"Вы записали {text!r}")
+
+
+@bot.on.message(text="Дз на завтра")
+async def hi_handler(message: Message, text: str):
+    await message.answer(f" {text!r}")
 
 
 bot.run_forever()
