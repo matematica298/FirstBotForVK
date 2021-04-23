12f394083809384efaa2b8ccb171634fc0602a4e
matematica298 <shop_spy@mail.ru>
2021-04-23 19:29:38 +0300

    Somethind edited

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ async def hi_handler(message: Message, text: str):
     dz = dz + "\n" + text
     await message.answer(f"Вы записали {text!r}")
 
-@bot.on.chat_message(text=["Помощь", '@club181612389 Помощь'])
+@bot.on.chat_message(text=["Помощь","@club181612389 Помощь"])
 async def hi_handler(message: Message):
     # Создаем клавиатуру
     KEYBOARD1 = Keyboard(one_time=True)
@@ -53,7 +53,28 @@ async def hi_handler(message: Message):
     await message.answer("Пишешь это как 'Записать Физика п.58 читать и Дидактика стр 15 #3'")
     await message.answer("Вот тебе клавиатура...", keyboard=KEYBOARD1.get_json())
 
-@bot.on.chat_message(text=["Правила", "@club181612389 Правила"])
+@bot.on.chat_message(text="@club181612389 Помощь")
+async def hi_handler(message: Message):
+    # Создаем клавиатуру
+    KEYBOARD1 = Keyboard(one_time=True)
+
+    # Создаем клавишу
+    BUTTON = Text("Записать")
+    BUTTON1 = Text("Помощь")
+    BUTTON2 = Text("Правила")
+
+    # Добавляем клавишу в клавиатуру
+    KEYBOARD1.add(BUTTON)
+    KEYBOARD1.add(BUTTON1)
+    KEYBOARD1.add(BUTTON2)
+
+    await message.answer(f"Привет. Вот тебе полное объяснение команд:")
+    await message.answer("1)'Записать' используется для того, чтобы записать дз на завтра,"+"которое ты знаешь.")
+    await message.answer("К примеру ты хочешь записать 'Физика п.58 читать и Дидактика стр 15 #3'")
+    await message.answer("Пишешь это как 'Записать Физика п.58 читать и Дидактика стр 15 #3'")
+    await message.answer("Вот тебе клавиатура...", keyboard=KEYBOARD1.get_json())
+
+@bot.on.chat_message(text="Правила")
 async def hi_handler(message: Message):
     # Создаем клавиатуру
     KEYBOARD1 = Keyboard(one_time=True)
@@ -74,7 +95,33 @@ async def hi_handler(message: Message):
     await message.answer("3. Если вы имеете хоть какое-то ВЕРНОЕ дз, тогда нажмите помощь и добавьте его")
     await message.answer("Вот тебе клавиатура...", keyboard=KEYBOARD1.get_json())
 
-@bot.on.chat_message(text=["Дз на завтра", "@club181612389 Дз на завтра"])
+@bot.on.chat_message(text="@club181612389 Правила")
+async def hi_handler(message: Message):
+    # Создаем клавиатуру
+    KEYBOARD1 = Keyboard(one_time=True)
+
+    # Создаем клавишу
+    BUTTON = Text("Дз на завтра")
+    BUTTON1 = Text("Помощь")
+    BUTTON2 = Text("Правила")
+
+    # Добавляем клавишу в клавиатуру
+    KEYBOARD1.add(BUTTON)
+    KEYBOARD1.add(BUTTON1)
+    KEYBOARD1.add(BUTTON2)
+
+    await message.answer(f"Правила просты:")
+    await message.answer("1. Запрещено спамить командами")
+    await message.answer("2. Запрещено писать Всякую фигню в дз")
+    await message.answer("3. Если вы имеете хоть какое-то ВЕРНОЕ дз, тогда нажмите помощь и добавьте его")
+    await message.answer("Вот тебе клавиатура...", keyboard=KEYBOARD1.get_json())
+
+@bot.on.chat_message(text="Дз на завтра")
+async def hi_handler(message: Message):
+    global dz
+    await message.answer(dz)
+
+@bot.on.chat_message(text="@club181612389 Дз на завтра")
 async def hi_handler(message: Message):
     global dz
     await message.answer(dz)
