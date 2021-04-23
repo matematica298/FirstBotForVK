a90725b7ed933f5d520364a39826995c82f3a870
matematica298 <shop_spy@mail.ru>
2021-04-23 19:07:42 +0300

    Somethind edited

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ bot = Bot(token="042069c40f987a3c04a68c1524d19484d56d9089f8ca2dfd84bbd93e05d50cb
 bot.labeler.vbml_ignore_case = True
 
 
-@bot.on.message(text=["привет", 'ку'])
+@bot.on.chat_message(text=["привет", 'ку'])
 async def hi_handler(message: Message):
     # Создаем клавиатуру
     KEYBOARD = Keyboard(one_time=True)
@@ -23,10 +23,7 @@ async def hi_handler(message: Message):
 
     await message.answer("Привет", keyboard=KEYBOARD.get_json())
 
-
 dz = ''
-
-
 @bot.on.message(text="Записать <text>")
 async def hi_handler(message: Message, text: str):
     global dz
@@ -41,15 +38,38 @@ async def hi_handler(message: Message):
     # Создаем клавишу
     BUTTON = Text("Записать")
     BUTTON1 = Text("Помощь")
+    BUTTON2 = Text("Правила")
 
     # Добавляем клавишу в клавиатуру
     KEYBOARD1.add(BUTTON)
     KEYBOARD1.add(BUTTON1)
+    KEYBOARD1.add(BUTTON2)
 
     await message.answer(f"Привет. Вот тебе полное объяснение команд:")
     await message.answer("1)'Записать' используется для того, чтобы записать дз на завтра,"+"которое ты знаешь.")
     await message.answer("К примеру ты хочешь записать 'Физика п.58 читать и Дидактика стр 15 #3'")
     await message.answer("Пишешь это как 'Записать Физика п.58 читать и Дидактика стр 15 #3'")
+    await message.answer("Вот тебе клавиатура...", keyboard=KEYBOARD1.get_json())
+
+@bot.on.message(text="Правила")
+async def hi_handler(message: Message):
+    # Создаем клавиатуру
+    KEYBOARD1 = Keyboard(one_time=True)
+
+    # Создаем клавишу
+    BUTTON = Text("Дз на завтра")
+    BUTTON1 = Text("Помощь")
+    BUTTON2 = Text("Правила")
+
+    # Добавляем клавишу в клавиатуру
+    KEYBOARD1.add(BUTTON)
+    KEYBOARD1.add(BUTTON1)
+    KEYBOARD1.add(BUTTON2)
+
+    await message.answer(f"Правила просты:")
+    await message.answer("1. Запрещено спамить командами")
+    await message.answer("2. Запрещено писать Всякую фигню в дз")
+    await message.answer("3. Если вы имеете хоть какое-то ВЕРНОЕ дз, тогда нажмите помощь и добавьте его")
     await message.answer("Вот тебе клавиатура...", keyboard=KEYBOARD1.get_json())
 
 @bot.on.message(text="Дз на завтра")
