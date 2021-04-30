d96e98a3abc1b7ae3803dfc651f21e9a89a49e5d
matematica298 <shop_spy@mail.ru>
2021-04-30 18:10:31 +0300

    Somethind edited

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ async def hi_handler(message: Message):
     KEYBOARD.row()
 
     await message.answer("Привет")
-    #await message.answer("Привет", keyboard=KEYBOARD.get_json())
+    await message.answer("Привет", keyboard=KEYBOARD.get_json())
 
 dz = ''
 
