ba711784e93ee87859f220e13be78b89a2cc9c63
matematica298 <shop_spy@mail.ru>
2021-04-30 18:34:09 +0300

    Somethind edited

--- main.py
+++ main.py
@@ -8,6 +8,37 @@ bot.labeler.vbml_ignore_case = True
 @bot.on.message(text=["привет", 'ку'])
 async def hi_handler(message: Message):
     # Создаем клавиатуру
+    #KEYBOARD = Keyboard(one_time=True)
+
+    # Создаем клавишу
+    #BUTTON = Text("Записать")
+    #BUTTON1 = Text("Помощь")
+    #BUTTON2 = Text("Правила")
+
+    # Добавляем клавишу в клавиатуру
+    #KEYBOARD.add(BUTTON)
+    #KEYBOARD.add(BUTTON1)
+    #KEYBOARD.add(BUTTON2)
+
+    # Перенос на новую строчку
+    #KEYBOARD.row()
+
+     await message.answer("Привет")
+    # await message.answer("Привет", keyboard=KEYBOARD.get_json())
+
+dz = ''
+
+@bot.on.message(text="Записать <text>")
+async def hi_handler(message: Message, text: str):
+    global dz
+    users_info = await bot.api.users.get(message.from_id)
+    dz = dz + "\n" + text + (" Это оставил: {}".format(users_info[0].first_name)) +(
+        "{}".format(users_info[0].last_name))
+    await message.answer(f"Вы записали {text!r}")
+
+@bot.on.chat_message(text="Помощь")
+async def hi_handler(message: Message):
+    # Создаем клавиатуру
     KEYBOARD = Keyboard(one_time=True)
 
     # Создаем клавишу
@@ -20,42 +51,14 @@ async def hi_handler(message: Message):
     KEYBOARD.add(BUTTON1)
     KEYBOARD.add(BUTTON2)
 
-    # Перенос на новую строчку
-    KEYBOARD.row()
+    # Получаем JSON-развертку клавиатуры
+    KEYBOARD = KEYBOARD.get_json()
 
-    # await message.answer("Привет")
-    await message.answer("Привет", keyboard=KEYBOARD.get_json())
-
-dz = ''
-
-@bot.on.message(text="Записать <text>")
-async def hi_handler(message: Message, text: str):
-    global dz
-    users_info = await bot.api.users.get(message.from_id)
-    dz = dz + "\n" + text + (" Это оставил: {}".format(users_info[0].first_name)) +(
-        "{}".format(users_info[0].last_name))
-    await message.answer(f"Вы записали {text!r}")
-
-# @bot.on.chat_message(text="Помощь")
-# async def hi_handler(message: Message):
-# Создаем клавиатуру
-# KEYBOARD1 = Keyboard(one_time=True)
-
-# Создаем клавишу
-# BUTTON = Text("Записать")
-# BUTTON1 = Text("Помощь")
-# BUTTON2 = Text("Правила")
-
-# Добавляем клавишу в клавиатуру
-# KEYBOARD1.add(BUTTON)
-# KEYBOARD1.add(BUTTON1)
-# KEYBOARD1.add(BUTTON2)
-
-#  await message.answer(f"Привет. Вот тебе полное объяснение команд:")
-#  await message.answer("1)'Записать' используется для того, чтобы записать дз на завтра,"+"которое ты знаешь.")
-# await message.answer("К примеру ты хочешь записать 'Физика п.58 читать и Дидактика стр 15 #3'")
-# await message.answer("Пишешь это как 'Записать Физика п.58 читать и Дидактика стр 15 #3'")
-# await message.answer("Вот тебе клавиатура...", keyboard=KEYBOARD1.get_json())
+    await message.answer(f"Привет. Вот тебе полное объяснение команд:")
+    await message.answer("1)'Записать' используется для того, чтобы записать дз на завтра,"+"которое ты знаешь.")
+    await message.answer("К примеру ты хочешь записать 'Физика п.58 читать и Дидактика стр 15 #3'")
+    await message.answer("Пишешь это как 'Записать Физика п.58 читать и Дидактика стр 15 #3'")
+    await message.answer("Вот тебе клавиатура...", keyboard=KEYBOARD.get_json())
 
 
 @bot.on.message(text="Правила")
