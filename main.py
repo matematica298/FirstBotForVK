681da6d97d1cadf5dec797f1dda39bac505a1681
matematica298 <shop_spy@mail.ru>
2021-04-30 18:12:23 +0300

    Somethind edited

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ async def hi_handler(message: Message):
     # Перенос на новую строчку
     KEYBOARD.row()
 
-    await message.answer("Привет")
+   # await message.answer("Привет")
     await message.answer("Привет", keyboard=KEYBOARD.get_json())
 
 dz = ''
