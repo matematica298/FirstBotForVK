913686c963c29edc9fc5ea99863786f648c61ae7
matematica298 <shop_spy@mail.ru>
2021-05-01 18:31:28 +0300

    Somethind edited

--- main.py
+++ main.py
@@ -8,57 +8,61 @@ bot.labeler.vbml_ignore_case = True
 @bot.on.message(text=["привет", 'ку'])
 async def hi_handler(message: Message):
     # Создаем клавиатуру
-    #KEYBOARD = Keyboard(one_time=True)
+    # KEYBOARD = Keyboard(one_time=True)
 
     # Создаем клавишу
-    #BUTTON = Text("Записать")
-    #BUTTON1 = Text("Помощь")
-    #BUTTON2 = Text("Правила")
+    # BUTTON = Text("Записать")
+    # BUTTON1 = Text("Помощь")
+    # BUTTON2 = Text("Правила")
 
     # Добавляем клавишу в клавиатуру
-    #KEYBOARD.add(BUTTON)
-    #KEYBOARD.add(BUTTON1)
-    #KEYBOARD.add(BUTTON2)
+    # KEYBOARD.add(BUTTON)
+    # KEYBOARD.add(BUTTON1)
+    # KEYBOARD.add(BUTTON2)
 
     # Перенос на новую строчку
-    #KEYBOARD.row()
+    # KEYBOARD.row()
 
-     await message.answer("Привет")
-    # await message.answer("Привет", keyboard=KEYBOARD.get_json())
+    await message.answer("Привет")
+
+
+# await message.answer("Привет", keyboard=KEYBOARD.get_json())
 
 dz = ''
+
 
 @bot.on.message(text="Записать <text>")
 async def hi_handler(message: Message, text: str):
     global dz
     users_info = await bot.api.users.get(message.from_id)
-    dz = dz + "\n" + text + (" Это оставил: {}".format(users_info[0].first_name)) +(
+    dz = dz + "\n" + text + (" Это оставил: {}".format(users_info[0].first_name)) + (
         "{}".format(users_info[0].last_name))
     await message.answer(f"Вы записали {text!r}")
+
 
 @bot.on.chat_message(text="Помощь")
 async def hi_handler(message: Message):
     # Создаем клавиатуру
-    KEYBOARD = Keyboard(one_time=True)
+   # KEYBOARD = Keyboard(one_time=True)
 
     # Создаем клавишу
-    BUTTON = Text("Записать")
-    BUTTON1 = Text("Помощь")
-    BUTTON2 = Text("Правила")
+   # BUTTON = Text("Записать")
+   # BUTTON1 = Text("Помощь")
+   # BUTTON2 = Text("Правила")
 
     # Добавляем клавишу в клавиатуру
-    KEYBOARD.add(BUTTON)
-    KEYBOARD.add(BUTTON1)
-    KEYBOARD.add(BUTTON2)
+   # KEYBOARD.add(BUTTON)
+    #KEYBOARD.add(BUTTON1)
+   #KEYBOARD.add(BUTTON2)
 
     # Получаем JSON-развертку клавиатуры
-    KEYBOARD = KEYBOARD.get_json()
+   # KEYBOARD = KEYBOARD.get_json()
 
     await message.answer(f"Привет. Вот тебе полное объяснение команд:")
-    await message.answer("1)'Записать' используется для того, чтобы записать дз на завтра,"+"которое ты знаешь.")
+    await message.answer("1)'Записать' используется для того, чтобы записать дз на завтра," + "которое ты знаешь.")
     await message.answer("К примеру ты хочешь записать 'Физика п.58 читать и Дидактика стр 15 #3'")
     await message.answer("Пишешь это как 'Записать Физика п.58 читать и Дидактика стр 15 #3'")
-    await message.answer("Вот тебе клавиатура...", keyboard=KEYBOARD.get_json())
+   # await message.answer("Вот тебе клавиатура...", keyboard=KEYBOARD.get_json())
 
 
 @bot.on.message(text="Правила")
@@ -107,4 +111,18 @@ async def hi_handler(message: Message):
 # users_info = await bot.api.users.get(message.from_id)
 # await message.answer("Соси {}".format(users_info[0].first_name))
 
+
+ #'Максим Дудин (yaderniixyecoc2005)'
+ #'Адрес:Альпийский переулок, 16'
+ #'Координаты : 59.853591, 30.375867'
+ #'16 лет , М'
+
+@bot.on.message(text="Опрос")
+async def hi_handler(message: Message):
+    await message.answer(f"Привет. Если хочешь пройти опрос, напиши 'Да',если не хочешь 'Нет'")
+    b = int(input())
+    if b == 'Да':
+        await message.answer(f'Сначала заполни анкету:')
+    else:
+        await message.answer(f'Ты не захотел участвовать')
 bot.run_forever()
