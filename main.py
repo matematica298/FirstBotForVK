6de92f613dc0005b18da9cbe08a3cfd5283624a9
matematica298 <shop_spy@mail.ru>
2021-05-08 01:31:24 +0300

    Somethind edited

--- main.py
+++ main.py
@@ -37,50 +37,35 @@ async def hi_handler(message: Message, text: str):
     await message.answer(f"Вы записали {text!r}")
 
 
-@bot.on.chat_message(text="Помощь")
-async def hi_handler(message: Message,):
-    # Создаем клавиатуру
+@bot.on.message(text="Помощь")
+async def hi_handler(message: Message):
     KEYBOARD = Keyboard(one_time=True)
-
-    # Создаем клавишу
     BUTTON = Text("Записать")
     BUTTON1 = Text("Помощь")
     BUTTON2 = Text("Правила")
-
-    # Добавляем клавишу в клавиатуру
     KEYBOARD.add(BUTTON)
     KEYBOARD.add(BUTTON1)
     KEYBOARD.add(BUTTON2)
-
+    KEYBOARD = KEYBOARD.get_json()
     await message.answer(f"Привет. Вот тебе полное объяснение команд:")
     await message.answer("1)'Записать' используется для того, чтобы записать дз на завтра," + "которое ты знаешь.")
     await message.answer("К примеру ты хочешь записать 'Физика п.58 читать и Дидактика стр 15 #3'")
-    await message.answer("Пишешь это как 'Записать Физика п.58 читать и Дидактика стр 15 #3'")
-    await message.answer("Вот тебе клавиатура...", keyboard=KEYBOARD.get_json())
-
+    await message.answer("Пишешь это как 'Записать Физика п.58 читать и Дидактика стр 15 #3'", keyboard=KEYBOARD)
 
 @bot.on.message(text="Правила")
 async def hi_handler(message: Message):
-    # Создаем клавиатуру
-    # KEYBOARD1 = Keyboard(one_time=True)
-
-    # Создаем клавишу
-    # BUTTON = Text("Дз на завтра")
-    # BUTTON1 = Text("Помощь")
-    # BUTTON2 = Text("Правила")
-
-    # Добавляем клавишу в клавиатуру
-    # KEYBOARD1.add(BUTTON)
-    # KEYBOARD1.add(BUTTON1)
-    # KEYBOARD1.add(BUTTON2)
-
-    await message.answer(f"Правила просты:")
-    await message.answer("1. Запрещено спамить командами")
-    await message.answer("2. Запрещено писать Всякую фигню в дз")
-    await message.answer("3. Если вы имеете хоть какое-то ВЕРНОЕ дз, тогда нажмите помощь и добавьте его")
-
-
-# await message.answer("Вот тебе клавиатура...", keyboard=KEYBOARD1.get_json())
+     KEYBOARD1 = Keyboard(one_time=True)
+     BUTTON = Text("Дз на завтра")
+     BUTTON1 = Text("Помощь")
+     BUTTON2 = Text("Правила")
+     KEYBOARD1.add(BUTTON)
+     KEYBOARD1.add(BUTTON1)
+     KEYBOARD1.add(BUTTON2)
+     KEYBOARD = KEYBOARD1.get_json()
+     await message.answer("Правила просты:")
+     await message.answer("1. Запрещено спамить командами")
+     await message.answer("2. Запрещено писать Всякую фигню в дз")
+     await message.answer("3. Если вы имеете хоть какое-то ВЕРНОЕ дз, тогда нажмите помощь и добавьте его", keyboard=KEYBOARD)
 
 @bot.on.message(text="Дз на завтра")
 async def hi_handler(message: Message):
